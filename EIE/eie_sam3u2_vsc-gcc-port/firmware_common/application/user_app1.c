8dc5fb1d5d67762265a4c74525cdabfeed31c5a3
2BlackCats <903036002alex@gmail.com>
2024-10-03 20:02:31 -0600

    Added Blinking light functionality

--- EIE/eie_sam3u2_vsc-gcc-port/firmware_common/application/user_app1.c
+++ EIE/eie_sam3u2_vsc-gcc-port/firmware_common/application/user_app1.c
@@ -92,6 +92,7 @@ Promises:
 */
 void UserApp1Initialize(void)
 {
+  HEARTBEAT_OFF();
   /* If good initialization, set state to Idle */
   if( 1 )
   {
@@ -140,7 +141,25 @@ State Machine Function Definitions
 /* What does this state do? */
 static void UserApp1SM_Idle(void)
 {
-     
+  static u16 u16Counter = U16_COUNTER_PERIOD_MS;
+  static bool bToggle = FALSE;
+  u16Counter--;
+  if (u16Counter == 0)
+  {
+    u16Counter = U16_COUNTER_PERIOD_MS;
+
+    if (bToggle)
+    {
+      HEARTBEAT_OFF();
+      bToggle = FALSE;
+    }
+    else
+    {
+      HEARTBEAT_ON();
+      bToggle = TRUE;
+    }
+  }
+  
 } /* end UserApp1SM_Idle() */
      
 
